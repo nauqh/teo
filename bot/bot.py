2f1afd3aa9a28f4689975d900948afd4e3cbea3b
Nauqh <hodominhquan.work@gmail.com>
2024-05-09 23:00:54 +1000

    test(tests): add test for engine

--- bot/bot.py
+++ bot/bot.py
@@ -3,21 +3,21 @@ import hikari
 import lightbulb
 from datetime import datetime, timedelta
 import asyncio
+from dotenv import load_dotenv
+import os
 
-from .utils.config import settings
+load_dotenv()
 
-TOKEN = settings['TOKEN']
-GUILD = settings['GUILD']
-ADMIN = settings['ADMIN']
-FORUM_CHANNEL = settings['FORUM_CHANNEL']
+TOKEN = os.getenv("TOKEN")
+GUILD = int(os.getenv("GUILD"))
+ADMIN = int(os.getenv("ADMIN"))
+FORUM_CHANNEL = int(os.getenv("FORUM_CHANNEL"))
 
-app = lightbulb.BotApp(
-    TOKEN,
-    intents=hikari.Intents.ALL,
-    default_enabled_guilds=GUILD,
-    help_slash_command=True,
-    banner=None
-)
+app = lightbulb.BotApp(TOKEN,
+                       intents=hikari.Intents.ALL,
+                       default_enabled_guilds=GUILD,
+                       help_slash_command=True,
+                       banner=None)
 
 
 @app.listen(hikari.StartingEvent)
@@ -29,15 +29,17 @@ async def check_threads():
     CHECK_INTERVAL = 900  # 15min
     while True:
         await asyncio.sleep(CHECK_INTERVAL)
-        threads = [thread
-                   for thread in await app.rest.fetch_active_threads(GUILD)
-                   if isinstance(thread, hikari.GuildThreadChannel)
-                   and thread.parent_id == FORUM_CHANNEL
-                   and thread.created_at.date() in [datetime.now().date(),
-                                                    datetime.now().date() - timedelta(days=1)]]
+        threads = [
+            thread for thread in await app.rest.fetch_active_threads(GUILD)
+            if isinstance(thread, hikari.GuildThreadChannel) and
+            thread.parent_id == FORUM_CHANNEL and thread.created_at.date() in
+            [datetime.now().date(),
+             datetime.now().date() - timedelta(days=1)]
+        ]
         for thread in threads:
-            if thread.approximate_member_count <= 1:
-                messages: list[hikari.Message] = await thread.fetch_history()
+            messages: list[hikari.Message] = await thread.fetch_history()
+            members = {message.author.id for message in messages}
+            if len(members) <= 1:
                 author = await app.rest.fetch_member(GUILD, thread.owner_id)
                 attachments = [att.url for att in messages[0].attachments]
 
@@ -46,12 +48,20 @@ async def check_threads():
                     description=messages[0].content,
                     color="#118ab2",
                     url=f"https://discord.com/channels/{thread.guild_id}/{thread.id}"
-                ).set_footer(
-                    text=f"Posted by {author.global_name}",
-                    icon=author.avatar_url
-                ).set_image(attachments[0])
+                ).set_footer(text=f"Posted by {author.global_name}",
+                             icon=author.avatar_url)
 
-                await app.rest.create_message(1237424754739253279, content=f"⚠️⏰<@&1194665960376901773> <@1214095592372969505> <@1214041211183038504> this thread remains unresolved for more than 5s", embed=embed)
+                if attachments:
+                    embed.set_image(attachments[0])
+
+                await app.rest.create_message(
+                    1237424754739253279,
+                    content=(
+                        "<@&1194665960376901773> "
+                        "<@1214095592372969505> "
+                        "<@815706256463364116> "
+                        "this thread remains unresolved for more than 15min"),
+                    embed=embed)
 
 
 @app.listen(hikari.StartedEvent)
