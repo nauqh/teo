549aee73e401a580031d527a4d23447b74c7e0d9
Nauqh <hodominhquan.work@gmail.com>
2024-05-14 02:56:48 +1000

    fix(env): add env config

--- bot/bot.py
+++ bot/bot.py
@@ -2,19 +2,26 @@ import bot
 import hikari
 import lightbulb
 from datetime import datetime
-from bot.config import Config as cf
+import pytz
+from bot.config import Config
 from bot.utils.embed import noti_embed
 
 import asyncio
 
+cf = Config('dev')
 
 app = lightbulb.BotApp(
     cf.TOKEN,
     intents=hikari.Intents.ALL,
-    default_enabled_guilds=cf.GUILD,
+    default_enabled_guilds=[cf.DATA.GUILD, cf.FSW.GUILD],
     help_slash_command=True,
     banner=None
 )
+
+
+def today():
+    gmt7 = pytz.timezone('Asia/Bangkok')
+    return datetime.now().astimezone(gmt7).date()
 
 
 @app.listen(hikari.StartingEvent)
@@ -22,25 +29,25 @@ async def on_starting(event: hikari.StartingEvent) -> None:
     app.d.admin = await app.rest.fetch_user(cf.ADMIN)
 
 
-def is_today(dt: datetime) -> bool:
-    return dt.date() == datetime.now().date()
-
-
-async def check_threads():
-    CHECK_INTERVAL = 1800
+async def check_threads(
+    guild: int,
+    forum_channel: int,
+    staff_channel: int
+):
+    CHECK_INTERVAL = 5
     while True:
         await asyncio.sleep(CHECK_INTERVAL)
         threads = [
-            thread for thread in await app.rest.fetch_active_threads(cf.GUILD)
+            thread for thread in await app.rest.fetch_active_threads(guild)
             if isinstance(thread, hikari.GuildThreadChannel) and
-            thread.parent_id == cf.FORUM_CHANNEL and
-            thread.created_at.date() == datetime.now().date()
+            thread.parent_id == forum_channel and
+            thread.created_at.date() == today()
         ]
         for thread in threads:
             messages: list[hikari.Message] = await thread.fetch_history()
             members = {message.author.id for message in messages}
             if len(members) <= 1:
-                author = await app.rest.fetch_member(cf.GUILD, thread.owner_id)
+                author = await app.rest.fetch_member(guild, thread.owner_id)
                 attachments = [att.url for att in messages[0].attachments]
 
                 embed = noti_embed(thread.name, messages[0].content,
@@ -50,39 +57,37 @@ async def check_threads():
                     embed.set_image(attachments[0])
 
                 await app.rest.create_message(
-                    1237424754739253279,
+                    staff_channel,
                     content=(
                         "<@&1194665960376901773> "
-                        "<@1214095592372969505> "
-                        "<@1214041211183038504> "
+                        f"<@{cf.ADMIN}> "
                         "this thread remains unresolved for more than 15min"),
                     embed=embed
                 )
 
 
 async def check_exam_requests():
-    channel = await app.rest.fetch_messages(cf.EXAM_CHANNEL)
-    CHECK_INTERVAL = 1000
+    channel = await app.rest.fetch_channel(cf.DATA.EXAM_CHANNEL)
+    CHECK_INTERVAL = 5
     while True:
         await asyncio.sleep(CHECK_INTERVAL)
-        messages = await app.rest.fetch_messages(cf.EXAM_CHANNEL).take_while(
-            lambda message: message.created_at.date() == datetime.now().date()
+        messages = await app.rest.fetch_messages(cf.DATA.EXAM_CHANNEL).take_while(
+            lambda message: message.created_at.date() == today()
         )
 
         for message in messages:
-            author = await app.rest.fetch_member(cf.GUILD, message.author.id)
+            author = await app.rest.fetch_member(cf.DATA.GUILD, message.author.id)
             # Filter messages from learner (not have TA role) and has no reactions
             if 1194665960376901773 not in [role.id for role in author.get_roles()] and not message.reactions:
                 embed = noti_embed(
-                    message.name, message.content,
+                    channel.name, message.content,
                     f"https://discord.com/channels/{channel.guild_id}/{channel.id}", author)
 
                 await app.rest.create_message(
-                    1237424754739253279,
+                    cf.DATA.STAFF_CHANNEL,
                     content=(
                         "<@&1194665960376901773> "
-                        "<@1214095592372969505> "
-                        "<@1214041211183038504> "
+                        f"<@{cf.ADMIN}> "
                         "this exam request remains unresolved for more than 15min"),
                     embed=embed
                 )
@@ -90,7 +95,13 @@ async def check_exam_requests():
 
 @app.listen(hikari.StartedEvent)
 async def on_started(event: hikari.StartedEvent) -> None:
-    asyncio.create_task(check_threads())
+    # Check question center
+    asyncio.create_task(check_threads(
+        cf.DATA.GUILD, cf.DATA.FORUM_CHANNEL, cf.DATA.STAFF_CHANNEL))
+    asyncio.create_task(check_threads(
+        cf.FSW.GUILD, cf.FSW.FORUM_CHANNEL, cf.FSW.STAFF_CHANNEL))
+
+    # Check exam request
     asyncio.create_task(check_exam_requests())
 
 
@@ -110,5 +121,6 @@ def run() -> None:
         activity=hikari.Activity(
             name=f"v{bot.__version__}",
             type=hikari.ActivityType.LISTENING,
-            state="💡teodocs | /help")
+            state="💡teodocs | /help"
+        )
     )
--- bot/config.py
+++ bot/config.py
@@ -5,9 +5,52 @@ load_dotenv()
 
 
 class Config:
-    TOKEN = os.getenv("TOKEN")
-    GUILD = int(os.getenv("GUILD"))
-    ADMIN = int(os.getenv("ADMIN"))
-    FORUM_CHANNEL = int(os.getenv("FORUM_CHANNEL"))
-    EXAM_CHANNEL = int(os.getenv("EXAM_CHANNEL"))
-    STAFF_CHANNEL = int(os.getenv("STAFF_CHANNEL"))
+    config_mapping = {
+        'dev': {'primary': 'hym', 'secondary': 'monash'},
+        'prod': {'primary': 'data', 'secondary': 'fsw'}
+    }
+
+    def __init__(self, mode: str):
+
+        primary, secondary = self.config_mapping.get(mode, {}).values()
+        self.TOKEN = os.getenv('TOKEN_DEV' if mode == 'dev' else 'PROD')
+        self.ADMIN = int(os.getenv('ADMIN'))
+        self._load_config(primary)
+        self._load_config(secondary)
+
+    def _load_config(self, config_name: str):
+        config = globals().get(f"{config_name.capitalize()}Config")
+        setattr(self, config_name.upper(), config())
+
+
+class BaseConfig:
+    def __init__(self, prefix):
+        self.GUILD = int(os.getenv(f"{prefix}_GUILD"))
+        self.FORUM_CHANNEL = int(os.getenv(f"{prefix}_FORUM_CHANNEL"))
+        self.EXAM_CHANNEL = int(os.getenv(f"{prefix}_EXAM_CHANNEL"))
+        self.STAFF_CHANNEL = int(os.getenv(f"{prefix}_STAFF_CHANNEL"))
+
+
+class HymConfig(BaseConfig):
+    def __init__(self):
+        super().__init__('HYM')
+
+
+class MonashConfig(BaseConfig):
+    def __init__(self):
+        super().__init__('MONASH')
+
+
+class DataConfig(BaseConfig):
+    def __init__(self):
+        super().__init__('DATA')
+
+
+class FswConfig(BaseConfig):
+    def __init__(self):
+        super().__init__('FSW')
+
+
+if __name__ == "__main__":
+    cf = Config('dev')
+    print(cf.HYM.GUILD)
