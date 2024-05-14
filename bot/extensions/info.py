2e8d39ee7190b95359c858369e622b41b4f046d3
Nauqh <hodominhquan.work@gmail.com>
2024-05-14 15:32:10 +1000

    fix(env): add env config

--- bot/extensions/info.py
+++ bot/extensions/info.py
@@ -81,8 +81,9 @@ async def get_info(ctx: lightbulb.Context):
     embed = (
         Embed(
             title=f"✨ User Info",
-            colour="#06d6a0",
+            colour="#118ab2",
             url="https://teodocs.vercel.app/",
+            description="T.e.o is a multifunctional bot designed to enhance forum management. With its advanced moderation and logging capabilities, it monitors forum activity, promoting moderator accountability and easing the workload for staff members by automating query filtering.",
             timestamp=datetime.now().astimezone(pytz.timezone('Asia/Ho_Chi_Minh'))
         )
         .set_thumbnail(bot.avatar_url)
