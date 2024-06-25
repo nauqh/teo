f012e8f7b9b6f25a36ba8913a3bbd80ed0284b3a
Nauqh <hodominhquan.work@gmail.com>
2024-06-26 00:15:55 +0700

    docs(jobs): add comment

--- bot/extensions/jobs.py
+++ bot/extensions/jobs.py
@@ -112,11 +112,11 @@ async def on_starting(event: hikari.StartingEvent) -> None:
     # Schedule job_post to run every 30 seconds
     plugin.app.d.scheduler = AsyncIOScheduler(timezone='Asia/Ho_Chi_Minh')
     plugin.app.d.scheduler.start()
-    plugin.app.d.scheduler.add_job(job_post, 'cron', second=30, args=[
+    plugin.app.d.scheduler.add_job(job_post, 'cron', second=20, args=[
                                    "https://topdev.vn/viec-lam-it/data-analytics-intern-fresher-junior-kt202", 1255062099118395454])
-    plugin.app.d.scheduler.add_job(job_post, 'cron', second=30, args=[
+    plugin.app.d.scheduler.add_job(job_post, 'cron', second=50, args=[
                                    "https://topdev.vn/viec-lam-it/react-javascript-kt7367,22", 1255068486573625394])
-    plugin.app.d.scheduler.add_job(job_post_itviec, 'cron', second=30, args=[
+    plugin.app.d.scheduler.add_job(job_post_itviec, 'cron', second=20, args=[
                                    "https://itviec.com/it-jobs/data-analyst", 1255062099118395454])
-    plugin.app.d.scheduler.add_job(job_post_itviec, 'cron', second=30, args=[
+    plugin.app.d.scheduler.add_job(job_post_itviec, 'cron', second=50, args=[
         "https://itviec.com/it-jobs/reactjs-javascript", 1255068486573625394])
