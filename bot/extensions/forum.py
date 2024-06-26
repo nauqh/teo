26347d4200813eb87e18b5b9728b10c0dbd6f47f
Nauqh <hodominhquan.work@gmail.com>
2024-06-27 00:22:36 +0700

    fix(forum): remove commands from forum

--- bot/extensions/forum.py
+++ bot/extensions/forum.py
@@ -26,60 +26,60 @@ def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(plugin)
 
 
-@plugin.command()
-@lightbulb.command('threads', 'Get all threads', auto_defer=True)
-@lightbulb.implements(lightbulb.SlashCommand)
-async def get_threads(ctx: lightbulb.Context):
-    data: list[hikari.GuildThreadChannel] = []
-    threads = [thread
-               for thread in await ctx.bot.rest.fetch_active_threads(GUILD)
-               if isinstance(thread, hikari.GuildThreadChannel)
-               and thread.parent_id == FORUM_CHANNEL
-               and thread.created_at.strftime("%Y-%m-%d") >= str(datetime.now().replace(day=1, month=5).date())]
-    msg = await ctx.bot.rest.create_message(ctx.channel_id, f"Extracting {len(threads)} threads")
+# @plugin.command()
+# @lightbulb.command('threads', 'Get all threads', auto_defer=True)
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def get_threads(ctx: lightbulb.Context):
+#     data: list[hikari.GuildThreadChannel] = []
+#     threads = [thread
+#                for thread in await ctx.bot.rest.fetch_active_threads(GUILD)
+#                if isinstance(thread, hikari.GuildThreadChannel)
+#                and thread.parent_id == FORUM_CHANNEL
+#                and thread.created_at.strftime("%Y-%m-%d") >= str(datetime.now().replace(day=1, month=5).date())]
+#     msg = await ctx.bot.rest.create_message(ctx.channel_id, f"Extracting {len(threads)} threads")
 
-    total = len(threads)
-    for count, thread in enumerate(threads, start=1):
-        messages: list[hikari.Message] = await thread.fetch_history()
-        data.append({
-            "id": thread.id,
-            "name": thread.name,
-            "created_at": thread.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-            "author_id": thread.owner_id,
-            "tags": thread.applied_tag_ids,
-            "messages": [{
-                "id": message.id,
-                "content": message.content,
-                "created_at": message.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                "author": message.author.id
-            } for message in messages]
-        })
-        await msg.edit(progress_bar(count/total))
+#     total = len(threads)
+#     for count, thread in enumerate(threads, start=1):
+#         messages: list[hikari.Message] = await thread.fetch_history()
+#         data.append({
+#             "id": thread.id,
+#             "name": thread.name,
+#             "created_at": thread.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+#             "author_id": thread.owner_id,
+#             "tags": thread.applied_tag_ids,
+#             "messages": [{
+#                 "id": message.id,
+#                 "content": message.content,
+#                 "created_at": message.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+#                 "author": message.author.id
+#             } for message in messages]
+#         })
+#         await msg.edit(progress_bar(count/total))
 
-    await ctx.respond("Extracted")
+#     await ctx.respond("Extracted")
 
-    df = pd.DataFrame(data)
-    df.to_csv("threads_draft.csv", index=False)
+#     df = pd.DataFrame(data)
+#     df.to_csv("threads_draft.csv", index=False)
 
-    await msg.edit(attachment="threads_draft.csv")
+#     await msg.edit(attachment="threads_draft.csv")
 
 
-@plugin.command()
-@lightbulb.command('members', 'Get all members', auto_defer=True)
-@lightbulb.implements(lightbulb.SlashCommand)
-async def get_members(ctx: lightbulb.Context):
-    data: list[hikari.User] = []
-    guild: hikari.Guild = ctx.get_guild()
-    for uid in guild.get_members():
-        member: hikari.User = guild.get_member(uid)
-        data.append({
-            "id": uid,
-            "name": member.global_name,
-            "is_bot": member.is_bot,
-            "roles": member.role_ids
-        })
+# @plugin.command()
+# @lightbulb.command('members', 'Get all members', auto_defer=True)
+# @lightbulb.implements(lightbulb.SlashCommand)
+# async def get_members(ctx: lightbulb.Context):
+#     data: list[hikari.User] = []
+#     guild: hikari.Guild = ctx.get_guild()
+#     for uid in guild.get_members():
+#         member: hikari.User = guild.get_member(uid)
+#         data.append({
+#             "id": uid,
+#             "name": member.global_name,
+#             "is_bot": member.is_bot,
+#             "roles": member.role_ids
+#         })
 
-    df = pd.DataFrame(data)
-    df.to_csv("members.csv", index=False)
+#     df = pd.DataFrame(data)
+#     df.to_csv("members.csv", index=False)
 
-    await ctx.respond(attachment="members.csv")
+#     await ctx.respond(attachment="members.csv")
