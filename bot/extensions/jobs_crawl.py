0f6bb655a7ab54cd8932c11fe031c46441146aa7
Nauqh <hodominhquan.work@gmail.com>
2024-07-12 14:50:04 +0700

    fix(models.py): remove urllib

--- bot/extensions/jobs_crawl.py
+++ bot/extensions/jobs_crawl.py
@@ -68,6 +68,6 @@ async def on_starting(event: hikari.StartingEvent) -> None:
     plugin.app.d.scheduler.start()
 
     plugin.app.d.scheduler.add_job(
-        run_script, 'cron', day_of_week='fri', hour=14, minute=47)
+        run_script, 'cron', day_of_week='fri', hour=14, minute=53)
     plugin.app.d.scheduler.add_job(
-        post_jobs, 'cron', day_of_week='fri', hour=14, minute=50)
+        post_jobs, 'cron', day_of_week='fri', hour=14, minute=55)
